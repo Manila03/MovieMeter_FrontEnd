5c1a79f19358a8f6cc0b6eb52d764d236d1900e2
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2025-01-04 12:11:24 -0300

    view Login_SignIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './views/Homepage'
 import './App.css'
+import Login_SignIn from "./views/Login_SignIn"
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/Login_SignIn' element={<Login_SignIn />} />
       </Routes>
     </>
   )
---
+++ src/views/Login_SignIn.jsx
@@ -0,0 +1,23 @@
+import "./Login_SignIn.css"
+
+const Login_SignIn = () => {
+    return (
+        <div className="container">
+        <div className="left-component">
+            <div>
+            <h2>Izquierda</h2>
+            <p>Este es el componente de la izquierda.</p>
+            </div>
+        </div>
+        <div className="right-component">
+            <div>
+            <h2>Derecha</h2>
+            <p>Este es el componente de la derecha.</p>
+            </div>
+        </div>
+        </div>
+    );
+    };
+
+export default Login_SignIn;
+
