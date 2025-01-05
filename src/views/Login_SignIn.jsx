6ac0e1fe1b7acfb3267f5b54c109bbc5a91d2848
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2025-01-05 11:41:27 -0300

    mejoras en diseño Login_SignIn

--- src/views/Login_SignIn.jsx
+++ src/views/Login_SignIn.jsx
@@ -1,23 +1,39 @@
-import "./Login_SignIn.css"
+import React, { useState } from "react";
+import "./Login_SignIn.css";
 
 const Login_SignIn = () => {
-    return (
-        <div className="container">
-        <div className="left-component">
-            <div>
-            <h2>Izquierda</h2>
-            <p>Este es el componente de la izquierda.</p>
-            </div>
-        </div>
-        <div className="right-component">
-            <div>
-            <h2>Derecha</h2>
-            <p>Este es el componente de la derecha.</p>
-            </div>
-        </div>
-        </div>
-    );
-    };
+  const [signIn, setSignIn] = useState(false); 
+
+  return (
+    <div className="container">
+      <div className="toggle-buttons">
+      </div>
+
+      <div className={`sign-in ${!signIn ? "hidden" : ""}`}>
+        <LeftComponent setSignIn={setSignIn} />
+      </div>
+
+      <div className={`login ${signIn ? "hidden" : ""}`}>
+        <RightComponent setSignIn={setSignIn} />
+      </div>
+    </div>
+  );
+};
+
+const LeftComponent = ({ setSignIn }) => (
+  <div>
+    <h2>Sign In</h2>
+    <p>Este es el componente de Sign In.</p>
+    <button onClick={() => setSignIn(false)}>Log In</button>
+  </div>
+);
+
+const RightComponent = ({ setSignIn }) => (
+  <div>
+    <h2>Log In</h2>
+    <p>Este es el componente de Log In.</p>
+    <button onClick={() => setSignIn(true)}>Sign In</button>
+  </div>
+);
 
 export default Login_SignIn;
-
