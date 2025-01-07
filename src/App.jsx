7249789a2306d544f3d4c15747c5a344fb553218
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2025-01-06 22:42:57 -0300

    incorporacion mail y password

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './views/Homepage'
 import './App.css'
-import Prueba2 from "./views/Prueba2";
 import Login_SignIn from "./views/Login_SignIn"
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/Login_SignIn' element={<Login_SignIn />} />
-        <Route path='/Prueba2' element={<Prueba2 />} />
       </Routes>
     </>
   )
--- src/views/Login_SignIn.jsx
+++ src/views/Login_SignIn.jsx
@@ -5,13 +5,66 @@ import Slider from "../components/Slider";
 //{condicion ? si_valor_true : si_valor_false}
 const Login_SignIn = () => {
   const [signIn, setSignIn] = useState(false); // false: muestra Login, true: muestra SignIn
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  const SignIn = () => (
+    <div>
+      <h2>Sign In</h2>
+      <p>Este es el componente de Sign In.</p>
+      <button onClick={() => setSignIn(false)}>Ir a Login</button>
+    </div>
+  );
+  
+  const Login = () => (
+    <div className="login-container">
+      <h2 className="titulo-login-signIn">Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Correo electrónico</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="password">Contraseña</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Iniciar sesión</button>
+      </form>
+      <button onClick={() => setSignIn(true)}>Ir a Sign In</button>
+    </div>
+
+  );
+  
+  const Imagenes = () => (
+    <div>
+        <Slider />
+      </div>
+  );
 
   return (
     <div className="container">
       <div
         className={`form-container ${signIn ? "sign-in-left" : "login-right"}`}
       >
-        {signIn ? <SignIn setSignIn={setSignIn} /> : <Login setSignIn={setSignIn} />}
+        {signIn ? <SignIn /> : <Login />}
       </div>
   
       <div
@@ -22,27 +75,5 @@ const Login_SignIn = () => {
     </div>
   );
 };
-
-const SignIn = ({ setSignIn }) => (
-  <div>
-    <h2>Sign In</h2>
-    <p>Este es el componente de Sign In.</p>
-    <button onClick={() => setSignIn(false)}>Ir a Login</button>
-  </div>
-);
-
-const Login = ({ setSignIn }) => (
-  <div>
-    <h2>Log In</h2>
-    <p>Este es el componente de Log In.</p>
-    <button onClick={() => setSignIn(true)}>Ir a Sign In</button>
-  </div>
-);
-
-const Imagenes = () => (
-  <div>
-      <Slider />
-    </div>
-);
 
 export default Login_SignIn;
